9348e23482e32d1a3bac45fc44e686674383d179
mate2000 <mateomon02@hotmail.com>
2022-03-29 19:34:45 -0500

    add Class Matriz
    - Added matrix function with operations

---
+++ main.py
@@ -0,0 +1,11 @@
+import Matriz
+
+matrix1 = ([1,2,3],[4,5,6],[7,8,9])
+matrix2 = ([1,2,3],[4,5,6],[7,8,9])
+
+mat = Matriz.MyClass(3, 3, matrix1, matrix2)
+
+print(mat.sum())
+print(mat.substract())
+print(mat.mul())
+print(mat.div())
